51181c6cf66f99668765066483162b6ca1b8e3d2
Ammar Khan <ammar.ikhan003@gmail.com>
2025-06-09 13:08:36 +0100

    More sensible events

--- src/components/views/Events.jsx
+++ src/components/views/Events.jsx
@@ -5,37 +5,37 @@ function Events() {
   const eventlist = [
     {
       EventID: 1,
-      EventName: "Summer Tech Conference",
-      Description: "Annual technology conference featuring the latest innovations",
-      Date: "2025-07-15",
+      EventName: "Christmas Party 2025",
+      Description: "A jolly good time",
+      Date: "2025-12-23",
       EventImageURL: "https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg"
     },
     {
       EventID: 2,
-      EventName: "Project Showcase",
-      Description: "Student projects presentation and networking event",
-      Date: "2025-08-20",
+      EventName: "Christmas Party 2026",
+      Description: "A jolly good time, but next year!",
+      Date: "2026-12-23",
       EventImageURL: "https://images.freeimages.com/images/small-previews/411/light-of-technology-1510575.jpg"
     },
     {
       EventID: 3,
-      EventName: "Web Dev Workshop",
-      Description: "Hands-on workshop for modern web development",
-      Date: "2025-09-05",
+      EventName: "Christmas Party 2027",
+      Description: "A jolly good time, but in 2 years!",
+      Date: "2027-12-23",
       EventImageURL: "https://images.freeimages.com/images/small-previews/64b/vla-1-1315506.jpg"
     },
     {
       EventID: 4,
-      EventName: "Data Science Summit",
-      Description: "Expert talks on advanced data analysis techniques",
-      Date: "2025-09-15",
+      EventName: "Christmas Party 2028",
+      Description: "A jolly good time, but in 3!",
+      Date: "2028-12-23",
       EventImageURL: "https://images.freeimages.com/images/small-previews/293/cable-4-1243085.jpg"
     },
     {
       EventID: 5,
-      EventName: "Cybersecurity Conference",
-      Description: "Latest trends in cybersecurity and defense",
-      Date: "2025-10-01",
+      EventName: "Christmas Party 2029",
+      Description: "A jolly good time, but in 4 years! You can never be too safe!",
+      Date: "2029-12-23",
       EventImageURL: "https://images.freeimages.com/images/small-previews/fa1/cable-5-1243077.jpg"
     }
   ];
