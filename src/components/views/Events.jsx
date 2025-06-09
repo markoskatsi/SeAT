7c5c2feb5eea08c9d937ad30a04f0ff09caa63f7
Ammar Khan <ammar.ikhan003@gmail.com>
2025-06-09 13:03:32 +0100

    Update Events.jsx

--- src/components/views/Events.jsx
+++ src/components/views/Events.jsx
@@ -4,84 +4,39 @@ import './Events.scss';
 function Events() {
   const eventlist = [
     {
-      ModuleID: 1,
-      ModuleName: "Games Programming",
-      ModuleCode: "CI2270",
-      ModuleLevel: 4,
-      ModuleLeaderID: 1,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg"
+      EventID: 1,
+      EventName: "Summer Tech Conference",
+      Description: "Annual technology conference featuring the latest innovations",
+      Date: "2025-07-15",
+      EventImageURL: "https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg"
     },
     {
-      ModuleID: 2,
-      ModuleName: "Individual Project",
-      ModuleCode: "CI7446",
-      ModuleLevel: 7,
-      ModuleLeaderID: 2,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/411/light-of-technology-1510575.jpg"
+      EventID: 2,
+      EventName: "Project Showcase",
+      Description: "Student projects presentation and networking event",
+      Date: "2025-08-20",
+      EventImageURL: "https://images.freeimages.com/images/small-previews/411/light-of-technology-1510575.jpg"
     },
     {
-      ModuleID: 3,
-      ModuleName: "Database Driven Web Applications",
-      ModuleCode: "CI6388",
-      ModuleLevel: 6,
-      ModuleLeaderID: 3,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/64b/vla-1-1315506.jpg"
+      EventID: 3,
+      EventName: "Web Dev Workshop",
+      Description: "Hands-on workshop for modern web development",
+      Date: "2025-09-05",
+      EventImageURL: "https://images.freeimages.com/images/small-previews/64b/vla-1-1315506.jpg"
     },
     {
-      ModuleID: 4,
-      ModuleName: "Advanced Data Modelling",
-      ModuleCode: "CI8502",
-      ModuleLevel: 6,
-      ModuleLeaderID: 4,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/293/cable-4-1243085.jpg"
+      EventID: 4,
+      EventName: "Data Science Summit",
+      Description: "Expert talks on advanced data analysis techniques",
+      Date: "2025-09-15",
+      EventImageURL: "https://images.freeimages.com/images/small-previews/293/cable-4-1243085.jpg"
     },
     {
-      ModuleID: 5,
-      ModuleName: "Ethical Hacking",
-      ModuleCode: "CI7572",
-      ModuleLevel: 7,
-      ModuleLeaderID: 5,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/fa1/cable-5-1243077.jpg"
-    },
-    {
-      ModuleID: 6,
-      ModuleName: "Network and Mobile Forensics",
-      ModuleCode: "CI5068",
-      ModuleLevel: 7, 
-      ModuleLeaderID: 6, 
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/930/towertv-3-1423238.jpg"
-    },
-    {
-      ModuleID: 7,
-      ModuleName: "Practical Data Analyst Skills",
-      ModuleCode: "CI9213",
-      ModuleLevel: 5,
-      ModuleLeaderID: 7,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg"
-    },
-    {
-      ModuleID: 8,
-      ModuleName: "Statistics in Practice",
-      ModuleCode: "CI5856",
-      ModuleLevel: 7,
-      ModuleLeaderID: 8,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/4e8/sala-de-parto-03-1432033.jpg"
-    },
-    {
-      ModuleID: 9,
-      ModuleName: "Internet Services and Protocols",
-      ModuleCode: "CI3651",
-      ModuleLevel: 7,
-      ModuleLeaderID: 9,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/6cc/monitor-2-1242535.jpg"
-    },
-    {
-      ModuleID: 10,
-      ModuleName: "High Level Game Development",
-      ModuleCode: "CI7952",
-      ModuleLevel: 5,
-      ModuleLeaderID: null,
-      ModuleImageURL: "https://images.freeimages.com/images/small-previews/402/rocket-in-the-museum-1450195.jpg"
+      EventID: 5,
+      EventName: "Cybersecurity Conference",
+      Description: "Latest trends in cybersecurity and defense",
+      Date: "2025-10-01",
+      EventImageURL: "https://images.freeimages.com/images/small-previews/fa1/cable-5-1243077.jpg"
     }
   ];
 
@@ -92,11 +47,12 @@ function Events() {
           {
             eventlist.map((event)=>{
               return (
-                <div className="eventCard" key={event.ModuleCode}>
+                <div className="eventCard" key={event.EventID}>
                   <Card>
-                    <p>{event.ModuleCode}</p>
-                    <p>{event.ModuleName}</p>
-                    <img src={event.ModuleImageURL}/>
+                    <h3>{event.EventName}</h3>
+                    <p>{event.Description}</p>
+                    <p>Date: {new Date(event.Date).toLocaleDateString()}</p>
+                    <img src={event.EventImageURL} alt={event.EventName}/>
                   </Card>
                 </div>
               )
