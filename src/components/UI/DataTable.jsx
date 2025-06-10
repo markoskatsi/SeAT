598f1f4266e1bbcd3fda82c7dc4ba71144467682
Harry <97326280+DebuggingHarry@users.noreply.github.com>
2025-06-10 17:57:33 +0100

    Data table and components styled
    Co-authored-by: Yeahhina <yeahhina@users.noreply.github.com>
    
    Data table and components styled.

--- src/components/UI/DataTable.jsx
+++ src/components/UI/DataTable.jsx
@@ -1,11 +1,4 @@
+import "./DataTable.scss";
 export function TableContainer(props) {
   return <div className="tableContainer">{props.children}</div>;
-}
-
-export function Table(props) {
-  return <table className="table">{props.children}</table>;
-}
-
-export function TableRow(props) {
-  return <tr className="tableRow">{props.children}</tr>;
 }
--- src/components/views/Employees.jsx
+++ src/components/views/Employees.jsx
@@ -1,5 +1,5 @@
 import "./Employees.scss";
-import { Table, TableRow, TableContainer } from "../UI/DataTable.jsx";
+import { TableContainer } from "../UI/DataTable.jsx";
 
 function Employees() {
   const employeelist = [
@@ -109,31 +109,36 @@ function Employees() {
     <>
       <h1>Employee</h1>
       <TableContainer>
-        <Table>
-          <th>User ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>D.O.B</th>
-          <th>Gender</th>
-          <th>UserRole</th>
-          <th>Department</th>
-          <th>Title</th>
-
-          {employeelist.map((employee) => {
-            return (
-              <TableRow key={employee.UserID}>
-                <td>{employee.UserID}</td>
-                <td>{employee.UserFirstname}</td>
-                <td>{employee.UserLastname}</td>
-                <td>{employee.UserDateOfBirth}</td>
-                <td>{employee.UserGender}</td>
-                <td>{employee.UserRole}</td>
-                <td>{employee.UserJobDepartment}</td>
-                <td>{employee.UserTitle}</td>
-              </TableRow>
-            );
-          })}
-        </Table>
+        <table>
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>D.O.B</th>
+              <th>Gender</th>
+              <th>UserRole</th>
+              <th>Department</th>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employeelist.map((employee) => {
+              return (
+                <tr key={employee.UserID}>
+                  <td>{employee.UserID}</td>
+                  <td>{employee.UserFirstname}</td>
+                  <td>{employee.UserLastname}</td>
+                  <td>{employee.UserDateOfBirth}</td>
+                  <td>{employee.UserGender}</td>
+                  <td>{employee.UserRole}</td>
+                  <td>{employee.UserJobDepartment}</td>
+                  <td>{employee.UserTitle}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </TableContainer>
     </>
   );
