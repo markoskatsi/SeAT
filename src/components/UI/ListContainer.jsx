7516154209dd34bda2d71fbef1df9126cb241360
Harry <97326280+DebuggingHarry@users.noreply.github.com>
2025-06-15 20:49:07 +0100

    api testing employees
    Testing api. Employees still need listing.

---
+++ src/components/UI/ListContainer.jsx
@@ -0,0 +1,10 @@
+import "./ListContainer.scss";
+export function ListContainer(props) {
+  return <div className="listContainer">{props.children}</div>;
+}
+export function HeaderContainer(props) {
+  return <div className="headerContainer">{props.children}</div>;
+}
+export function EmployeeItem(props) {
+  return <div className="headerContainer">{props.children}</div>;
+}
