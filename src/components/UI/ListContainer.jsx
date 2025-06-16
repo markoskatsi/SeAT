23a45698fff876ea15061e38cfa6abbb1811a4b9
Harry <97326280+DebuggingHarry@users.noreply.github.com>
2025-06-16 21:44:49 +0100

    Api now fetches employees
    API now correctly fetches and populates the employee view
    
    Co-Authored-By: Yeahhina <138409780+yeahhina@users.noreply.github.com>

--- src/components/UI/ListContainer.jsx
+++ src/components/UI/ListContainer.jsx
@@ -5,6 +5,19 @@ export function ListContainer(props) {
 export function HeaderContainer(props) {
   return <div className="headerContainer">{props.children}</div>;
 }
-export function EmployeeItem(props) {
-  return <div className="headerContainer">{props.children}</div>;
+
+export function EmployeeItem({ employee }) {
+  return (
+    <div className="employeeItem">
+      <p>{employee.UserID}</p>
+      <p>{employee.UserFirstname}</p>
+      <p>{employee.UserLastname}</p>
+      <p>{employee.UserDateofbirth}</p>
+      {/* <img src={employee.UserImageURL} /> */}
+      {/* <p>{employee.UserUsertypeID}</p>
+      <p>{employee.UserRoleID}</p> */}
+      <p>{employee.UserUsertypeName}</p>
+      <p>{employee.UserRoleName}</p>
+    </div>
+  );
 }
