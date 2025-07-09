678394302b7fff2a372da35e100330ea38cfe334
Yeahhina <138409780+yeahhina@users.noreply.github.com>
2025-07-09 19:43:33 +0100

    Styling added to homepage
    Co-authored-by: Harry <DebuggingHarry@users.noreply.github.com>

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useAuth } from "../../auth/useAuth";
 import "./Header.scss";
 
 function Header() {
   const { loggedInUser, logout } = useAuth();
+  const location = useLocation();
+  console.log(location);
   return (
     <header>
+      {}
       {loggedInUser && (
         <div className="navItems-left">
           <NavLink className="navItem" to="/events">
@@ -18,13 +21,7 @@ function Header() {
       )}
       <h1 className="titleNav">Seat</h1>
 
-      {!loggedInUser ? (
-        <div className="navItems">
-          <NavLink className="navItem" to="/login">
-            Login
-          </NavLink>
-        </div>
-      ) : (
+      {loggedInUser && (
         <div className="navItems-right">
           <NavLink className="navItem" to="/" onClick={logout}>
             Logout
