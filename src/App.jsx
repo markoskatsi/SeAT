19d04bd4f3e87b0ec813de81a5321bc60a7f04b5
Ammar Khan <ammar.ikhan003@gmail.com>
2025-08-20 18:43:48 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import RegisterForm from "./auth/registerForm.jsx";
 import LoginForm from "./auth/LoginForm.jsx";
 import "./App.scss";
 import Login from "./components/views/Login.jsx";
+import { AuthProvider } from "./auth/useAuth.jsx";
+import SeatingArrangements from "./components/views/SeatingArrangements.jsx";
 
 function App() {
   return (
@@ -22,6 +24,9 @@ function App() {
             <Route path="/users" element={<Users />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/seating" element={<SeatingArrangements />} />
           </Routes>
         </Layout>
       </BrowserRouter>
